b24aac1e409c250a0d3f32c7c0e1d187b39cb089
BSRaghava <sai@farmonaut.com>
2024-05-30 19:36:12 +0530

    Updated omdb

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,3 @@
-// import React from 'react';
-// import { Container, Box, Typography, Button, Grid } from '@mui/material';
-// import { useNavigate } from 'react-router-dom';
-
-// const Home = () => {
-//   const navigate = useNavigate();
-
-//   return (
-//     <Container maxWidth="md">
-//       <Box mt={5}>
-//         <Typography variant="h4" gutterBottom>
-//           Welcome to Movie App
-//         </Typography>
-//         <Grid container spacing={2}>
-//           <Grid item>
-//             <Button variant="contained" color="primary" onClick={() => navigate('/my-lists')}>
-//               My Lists
-//             </Button>
-//           </Grid>
-//           <Grid item>
-//             <Button variant="contained" color="primary" onClick={() => navigate('/search')}>
-//               Search Movies
-//             </Button>
-//           </Grid>
-//         </Grid>
-//       </Box>
-//     </Container>
-//   );
-// };
-
-// export default Home;
 
 
 import React, { useContext } from 'react';
