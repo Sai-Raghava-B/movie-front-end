f60bcab2e609aff11cdc71c87e32dbc2325d891b
BSRaghava <sai@farmonaut.com>
2024-05-30 22:34:48 +0530

    Updated Readme

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# Getting Started with Create React App
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
 
@@ -14,57 +11,20 @@ Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 The page will reload when you make changes.\
 You may also see any lint errors in the console.
 
-### `npm test`
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+You have to login it will show directly when you run the apllication. You need to register to login
+After that  you can see 3 buttons list , search and signout .
+List is to show all the lists you have created . From the search page.
+In the search Page you have to search for movies and create a list you have to create a list before adding them.
+After searching for a movie you get a drop down under each movie if you want to add it into a list. You can choose the list accordingly.
+After go to the My lists page and check the page . Trt to refresh if you dont see anything.
+You will see all the lists and movies which you added and there is also a option for toogle to make the list public or private.
+And if you want you can delete the List.
 
-### `npm run build`
+For accesing the public lists with a link u need to have the id  Of it  this the path for it "/public/:id"   As it is a front end application you will not see any id . If you wnat you can connect to the backend and add a console log in te react file to get the id and check it accordingly.
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+I will be sharing the backend part of it also. You can create a database and check for the id of list and try to access it.
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+This is the link for BACKEND GIT REPO:-      https://github.com/Sai-Raghava-B/Movie-Backend
 
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/components/MovieSearch.js
+++ src/components/MovieSearch.js
@@ -1,4 +1,180 @@
 
+// import React, { useState, useContext } from 'react';
+// import axios from 'axios';
+// import { Container, TextField, Button, Grid, Card, CardMedia, CardContent, Typography, Box, MenuItem, Select, InputLabel, FormControl, CircularProgress } from '@mui/material';
+// import AuthContext from '../context/AuthContext';
+
+// const MovieSearch = () => {
+//   const [query, setQuery] = useState('');
+//   const [movies, setMovies] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const {user, movieLists,setMovieLists } = useContext(AuthContext);
+//   const [selectedList, setSelectedList] = useState('');
+//   const [newListName, setNewListName] = useState('');
+//   const [creatingList, setCreatingList] = useState(false);
+//   const [isPublic, setIsPublic] = useState(false);
+
+//   const searchMovies = async () => {
+//     setLoading(true);
+//     try {
+//       const response = await axios.get(`https://www.omdbapi.com/?s=${query}&apikey=${process.env.REACT_APP_OMDB_API_KEY}`);
+//       setMovies(response.data.Search || []);
+//     } catch (error) {
+//       console.error('Error searching movies', error);
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+
+
+//   const addToMovieList = async (selectedMovie) => {
+//     // console.log(selectedList)
+//         if (!selectedList) {
+//           alert('Please select a list first.');
+//           return;
+//         }
+    
+//         const movie = {
+//           imdbID: selectedMovie.imdbID,
+//           title: selectedMovie.Title,
+//           year: selectedMovie.Year,
+//           poster: selectedMovie.Poster,
+//         };
+    
+//         try {
+//           await axios.post(`${process.env.REACT_APP_API_URL}/movies/add`, 
+//             {
+//               listId: selectedList,
+//               movie: movie,
+//             }, 
+//             {
+//               headers: { token: `${user.token}` },
+//             }
+//           );
+//           alert('Movie added successfully!');
+//         } catch (error) {
+//           console.log(error)
+//           alert('Failed to add movie. (Or) Maybe the movie is already present');
+//         }
+//   };
+    
+//     const createNewList = async () => {
+//       try {
+//         const res = await axios.post(`${process.env.REACT_APP_API_URL}/movies/`, 
+//           { name: newListName, isPublic }, 
+//           {
+//             headers: { token: `${user.token}` },
+//           }
+//         );
+//         setMovieLists((prevLists) => [...prevLists, res.data]);
+//         setNewListName('');
+//         setIsPublic(false);
+//         setSelectedList(res.data._id);
+//         alert('List created successfully!');
+//       } catch (error) {
+//         console.error(error);
+//         alert('Failed to create list.');
+//       }
+//     };
+
+
+//   return (
+//     <Container maxWidth="lg">
+//       <Box mt={5} mb={3}>
+//         <Typography variant="h4" gutterBottom>Search Movies</Typography>
+//         <Grid container spacing={2} alignItems="center">
+//           <Grid item xs={12} sm={8}>
+//             <TextField
+//               fullWidth
+//               label="Search for a movie"
+//               variant="outlined"
+//               value={query}
+//               onChange={(e) => setQuery(e.target.value)}
+//             />
+//           </Grid>
+//           <Grid item xs={12} sm={4}>
+//             <Button variant="contained" color="primary" onClick={searchMovies} fullWidth>Search</Button>
+//           </Grid>
+//         </Grid>
+//       </Box>
+//       {loading ? (
+//         <Box display="flex" justifyContent="center" mt={4}>
+//           <CircularProgress />
+//         </Box>
+//       ) : (
+//         <Grid container spacing={3}>
+//           {movies.map((movie) => (
+//             <Grid item xs={12} sm={6} md={4} lg={3} key={movie.imdbID}>
+//               <Card>
+//                 <CardMedia
+//                   component="img"
+//                   alt={movie.Title}
+//                   height="300"
+//                   image={movie.Poster}
+//                 />
+//                 <CardContent>
+//                   <Typography variant="h6">{movie.Title}</Typography>
+//                   <FormControl fullWidth>
+//                     <InputLabel id="select-list-label">Add to List</InputLabel>
+//                     <Select
+//                       labelId="select-list-label"
+//                       value={selectedList}
+//                       onChange={(e) => setSelectedList(e.target.value)}
+//                       variant="outlined"
+//                     >
+//                       {movieLists.map((list) => (
+//                         <MenuItem key={list._id} value={list._id}>{list.name}</MenuItem>
+//                       ))}
+//                     </Select>
+//                   </FormControl>
+//                   <Box mt={2}>
+//                     <Button
+//                       variant="contained"
+//                       color="secondary"
+//                       onClick={() => addToMovieList(movie)}
+//                       fullWidth
+//                     >
+//                       Add to List
+//                     </Button>
+//                   </Box>
+//                 </CardContent>
+//               </Card>
+//             </Grid>
+//           ))}
+//         </Grid>
+//       )}
+//       <Box mt={5}>
+//         <Typography variant="h5" gutterBottom>Create New List</Typography>
+//         <Grid container spacing={2} alignItems="center">
+//           <Grid item xs={12} sm={8}>
+//             <TextField
+//               fullWidth
+//               label="New List Name"
+//               variant="outlined"
+//               value={newListName}
+//               onChange={(e) => setNewListName(e.target.value)}
+//             />
+//           </Grid>
+//           <Grid item xs={12} sm={4}>
+//             <Button
+//               variant="contained"
+//               color="primary"
+//               onClick={createNewList}
+//               fullWidth
+//               disabled={creatingList}
+//             >
+//               {creatingList ? 'Creating...' : 'Create List'}
+//             </Button>
+//           </Grid>
+//         </Grid>
+//       </Box>
+//     </Container>
+//   );
+// };
+
+// export default MovieSearch;
+
+
 import React, { useState, useContext } from 'react';
 import axios from 'axios';
 import { Container, TextField, Button, Grid, Card, CardMedia, CardContent, Typography, Box, MenuItem, Select, InputLabel, FormControl, CircularProgress } from '@mui/material';
@@ -8,16 +184,11 @@ const MovieSearch = () => {
   const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
-  const {user, movieLists,setMovieLists } = useContext(AuthContext);
-  const [selectedList, setSelectedList] = useState('');
+  const { user, movieLists, setMovieLists } = useContext(AuthContext);
   const [newListName, setNewListName] = useState('');
   const [creatingList, setCreatingList] = useState(false);
-  // const [selectedLists, setSelectedLists] = useState('');
-  //   const [searchTerm, setSearchTerm] = useState('');
-  // const [results, setResults] = useState([]);
-  // const [selectedList, setSelectedList] = useState('');
-  // const [newListName, setNewListName] = useState('');
   const [isPublic, setIsPublic] = useState(false);
+  const [selectedLists, setSelectedLists] = useState({});
 
   const searchMovies = async () => {
     setLoading(true);
@@ -31,82 +202,62 @@ const MovieSearch = () => {
     }
   };
 
-  // const handleAddMovie = async (movie) => {
-  //   try {
-  //     await addMovieToList(selectedList, movie);
-  //   } catch (error) {
-  //     console.error('Error adding movie to list', error);
-  //   }
-  // };
-
   const addToMovieList = async (selectedMovie) => {
-    // console.log(selectedList)
-        if (!selectedList) {
-          alert('Please select a list first.');
-          return;
-        }
-    
-        const movie = {
-          imdbID: selectedMovie.imdbID,
-          title: selectedMovie.Title,
-          year: selectedMovie.Year,
-          poster: selectedMovie.Poster,
-        };
-    
-        try {
-          await axios.post(`${process.env.REACT_APP_API_URL}/movies/add`, 
-            {
-              listId: selectedList,
-              movie: movie,
-            }, 
-            {
-              headers: { token: `${user.token}` },
-            }
-          );
-          alert('Movie added successfully!');
-        } catch (error) {
-          console.log(error)
-          alert('Failed to add movie. (Or) Maybe the movie is already present');
-        }
-  };
-    
-    const createNewList = async () => {
-      try {
-        const res = await axios.post(`${process.env.REACT_APP_API_URL}/movies/`, 
-          { name: newListName, isPublic }, 
-          {
-            headers: { token: `${user.token}` },
-          }
-        );
-        setMovieLists((prevLists) => [...prevLists, res.data]);
-        setNewListName('');
-        setIsPublic(false);
-        setSelectedList(res.data._id);
-        alert('List created successfully!');
-      } catch (error) {
-        console.error(error);
-        alert('Failed to create list.');
-      }
+    const selectedList = selectedLists[selectedMovie.imdbID];
+
+    if (!selectedList) {
+      alert('Please select a list first.');
+      return;
+    }
+
+    const movie = {
+      imdbID: selectedMovie.imdbID,
+      title: selectedMovie.Title,
+      year: selectedMovie.Year,
+      poster: selectedMovie.Poster,
     };
 
-    // const handleSelectChange = (movieId, listId) => {
-    //   setSelectedLists(prevState => ({
-    //     ...prevState,
-    //     [movieId]: listId
-    //   }));
-    // };
+    try {
+      await axios.post(`${process.env.REACT_APP_API_URL}/movies/add`, 
+        {
+          listId: selectedList,
+          movie: movie,
+        }, 
+        {
+          headers: { token: `${user.token}` },
+        }
+      );
+      alert('Movie added successfully!');
+    } catch (error) {
+      console.log(error)
+      alert('Failed to add movie. (Or) Maybe the movie is already present');
+    }
+  };
 
-  // const handleCreateList = async () => {
-  //   setCreatingList(true);
-  //   try {
-  //     await createList(newListName);
-  //     setNewListName('');
-  //   } catch (error) {
-  //     console.error('Error creating list', error);
-  //   } finally {
-  //     setCreatingList(false);
-  //   }
-  // };
+  const createNewList = async () => {
+    setCreatingList(true);
+    try {
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}/movies/`, 
+        { name: newListName, isPublic }, 
+        {
+          headers: { token: `${user.token}` },
+        }
+      );
+      setMovieLists((prevLists) => [...prevLists, res.data]);
+      setNewListName('');
+      setIsPublic(false);
+      alert('List created successfully!');
+    } catch (error) {
+      console.error(error);
+      alert('Failed to create list.');
+    } finally {
+      setCreatingList(false);
+    }
+  };
+
+  const handleListChange = (movieId, value) => {
+    setSelectedLists(prevState => ({ ...prevState, [movieId]: value }));
+  };
 
   return (
     <Container maxWidth="lg">
@@ -145,13 +296,11 @@ const MovieSearch = () => {
                 <CardContent>
                   <Typography variant="h6">{movie.Title}</Typography>
                   <FormControl fullWidth>
-                    <InputLabel id="select-list-label">Add to List</InputLabel>
+                    <InputLabel id={`select-list-label-${movie.imdbID}`}>Add to List</InputLabel>
                     <Select
-                      labelId="select-list-label"
-                      // value={selectedList}
-                      // onChange={(e) => handleSelectChange( e.target.value)}
-                      value={selectedList}
-                      onChange={(e) => setSelectedList(e.target.value)}
+                      labelId={`select-list-label-${movie.imdbID}`}
+                      value={selectedLists[movie.imdbID] || ''}
+                      onChange={(e) => handleListChange(movie.imdbID, e.target.value)}
                       variant="outlined"
                     >
                       {movieLists.map((list) => (
